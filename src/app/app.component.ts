798f9a47f7ff95896b28970fdbc58b5e882fa038
Bachir Ali Mahamadou Aminou <35920099+mb151@users.noreply.github.com>
2020-02-25 09:35:37 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,14 @@ import * as firebase from 'firebase';
 export class AppComponent {
   constructor(){
     var firebaseConfig = {
-      apiKey: "your API Firebase Key",
-      authDomain: "ebookform-2cabc.firebaseapp.com",
-      databaseURL: "https://ebookform-2cabc.firebaseio.com",
-      projectId: "ebookform-2cabc",
-      storageBucket: "ebookform-2cabc.appspot.com",
-      messagingSenderId: "1055244044686",
-      appId: "1:1055244044686:web:9e110ed6fa3953ddb4bcdf",
-      measurementId: "G-D4LLQKXFT1"
+      apiKey: "API_KEY",
+      authDomain: "",
+      databaseURL: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: "",
+      measurementId: ""
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
